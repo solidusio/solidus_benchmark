d203474f8c58591bff8c7c9fe726d829696881e4
John Hawthorn <john@stembolt.com>
2016-11-29 15:48:06 -0800

    Add notes and descriptions

--- benchmark.rb
+++ benchmark.rb
@@ -7,6 +7,9 @@ require 'ffaker'
 require 'spree/testing_support/factories'
 
 SolidusBenchmark.new "core/refresh_rates" do
+  description %q{This benchmark measures the speed of `ShippingMethod#refresh_rates`, which estimates and creates shipping rates for a shipment.}
+  note "As of Solidus 1.3, taxes are calculated as part of shipping rate estimation"
+
   setup do
     FactoryGirl.create(:order_with_line_items)
   end
@@ -16,8 +19,11 @@ SolidusBenchmark.new "core/refresh_rates" do
   end
 end
 
-[1, 10].each do |item_count|
-  SolidusBenchmark.new "core/order/update/checkout_with_#{item_count}" do
+[1, 2, 5].each do |item_count|
+  SolidusBenchmark.new "core/order/cart/update/cold_update_with_#{item_count}_items" do
+    description %Q{This benchmark measures the speed of `Order#update!` for an order with #{item_count} items. This test reloads the order from memory before each update.}
+    note "As of Solidus 2.1, taxes are calculated as part of every order.update!"
+
     setup do
       FactoryGirl.create(:order_with_line_items, line_items_count: item_count)
     end
