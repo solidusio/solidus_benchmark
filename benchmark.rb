0202d6643c4cf32b14f75e07089fc9fa46eb12b2
John Hawthorn <john@stembolt.com>
2016-11-29 15:48:17 -0800

    Add hot order update benchmark

--- benchmark.rb
+++ benchmark.rb
@@ -32,4 +32,17 @@ end
       Spree::Order.first.update!
     end
   end
+
+  SolidusBenchmark.new "core/order/cart/update/hot_update_with_#{item_count}_items" do
+    description %Q{This benchmark measures the speed of `Order#update!` for an order with #{item_count} items. This test keeps the order in memory between updates.}
+    note "As of Solidus 2.1, taxes are calculated as part of every order.update!"
+
+    setup do
+      @order = FactoryGirl.create(:order_with_line_items, line_items_count: item_count).reload
+    end
+
+    measure do
+      @order.update!
+    end
+  end
 end
