4623a7f78170fc849200a8fdcb5efe96d290f13e
John Hawthorn <john@stembolt.com>
2016-11-29 16:53:56 -0800

    Test different numbers of shipping methods

--- suite/shipment/refresh_rates_benchmark.rb
+++ suite/shipment/refresh_rates_benchmark.rb
@@ -1,14 +1,19 @@
 require 'benchmark_helper'
 
-SolidusBenchmark.new "shipment/refresh_rates" do
-  description %q{This benchmark measures the speed of `ShippingMethod#refresh_rates`, which estimates and creates shipping rates for a shipment.}
-  note "As of Solidus 1.3, taxes are calculated as part of shipping rate estimation"
+[2, 10, 100].each do |n|
+  SolidusBenchmark.new "shipment/refresh_rates/#{n}_shipping_methods" do
+    description %Q{This benchmark measures the speed of `ShippingMethod#refresh_rates`, which estimates and creates shipping rates for a shipment. For this test there are #{n} shipping methods available}
+    note "As of Solidus 1.3, taxes are calculated as part of shipping rate estimation"
 
-  setup do
-    FactoryGirl.create(:order_with_line_items)
-  end
+    setup do
+      FactoryGirl.create(:order_with_line_items)
+      (n-1).times do
+        FactoryGirl.create(:shipping_method)
+      end
+    end
 
-  measure do
-    Spree::Shipment.last.refresh_rates
+    measure do
+      Spree::Shipment.last.refresh_rates
+    end
   end
 end
