9e9c66561133c746d75f85d7b54a1ef674d065b6
John Hawthorn <john@stembolt.com>
2016-12-06 14:44:42 -0800

    Avoid reusing the exact same order object
    This makes activerecord's dirty tracking go crazy

--- suite/order/update_benchmark.rb
+++ suite/order/update_benchmark.rb
@@ -19,7 +19,12 @@ require 'benchmark_helper'
     note "As of Solidus 2.1, taxes are calculated as part of every order.update!"
 
     setup do
-      @order = FactoryGirl.create(:order_with_line_items, line_items_count: item_count).reload
+      FactoryGirl.create(:order_with_line_items, line_items_count: item_count).reload
+    end
+
+    before do
+      @order = Spree::Order.first
+      @order.update!
     end
 
     measure do
